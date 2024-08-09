75042675192ba7b80e66285b40232ccf6bc59d37
Igor Barbosa <165736786+Igorsouza42@users.noreply.github.com>
2024-08-09 20:56:06 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿
+        // Solicita ao usuário que insira um número
+        Console.Write("Digite um número: ");
+        int numero = int.Parse(Console.ReadLine());
+
+        // Gera e exibe a tabuada de 1 a 10
+        Console.WriteLine($"Tabuada de {numero}:");
+        for (int i = 1; i <= 10; i++)
+        {
+            int resultado = numero * i;
+            Console.WriteLine($"{numero} x {i} = {resultado}");
+        }
+    
+
